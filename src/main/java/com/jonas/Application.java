3159fdb9a457590bd9a1aaa66d29497def268ed9
haiping <liuhaiping@hzyushi.cn>
2019-04-03 10:08:52 +0800

    修改db的访问

--- src/main/java/com/jonas/Application.java
+++ src/main/java/com/jonas/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 【 Spring Boot启动类 】
@@ -11,7 +12,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @author shenjy 2018/10/24
  */
 @EnableFeignClients
-@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/main/java/com/jonas/controller/UserController.java
+++ src/main/java/com/jonas/controller/UserController.java
@@ -37,5 +37,6 @@ public class UserController {
         user.setName(name);
         return userApiService.getUserObject(user);
     }
+
 }
 
