3cd0d346dcce4d30fe4bd9f98281db41a591d347
haiping <liuhaiping@hzyushi.cn>
2019-07-09 20:09:24 +0800

    修改

--- src/main/java/com/jonas/config/GlobalExceptionHandler.java
+++ src/main/java/com/jonas/config/GlobalExceptionHandler.java
@@ -6,6 +6,10 @@ import com.jonas.constant.BizException;
 import com.jonas.constant.JsonResult;
 import com.jonas.constant.SystemCode;
 import com.jonas.util.logging.SystemLogger;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,18 +22,25 @@ import java.util.List;
  * @author shenjy 2018/11/01
  */
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
-
+//    private Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     @ResponseBody
     @ExceptionHandler(Exception.class)
     public JsonResult handleException(Exception ex) {
         if (ex instanceof BizException) {
             Iterable iterable = Splitter.on(":").trimResults().omitEmptyStrings().split(ex.getMessage());
             List<String> item = Lists.newArrayList(iterable);
+            log.info("BizException:{},{}",item.get(1), item.get(2));
             return new JsonResult(item.get(1), item.get(2), null);
+        }else if (ex instanceof MissingServletRequestParameterException){
+            log.info("MissingServletRequestParameterException:{},{}",
+                    SystemCode.PARAM_ERROR.getCode(),ex.getMessage());
+            return new JsonResult(SystemCode.PARAM_ERROR.getCode(),ex.getMessage(),null);
         }
 
-        SystemLogger.error(ex, "handle exception");
+
+        log.error("handle exception:{}",ex);
         return new JsonResult(SystemCode.SERVER_ERROR);
     }
 }
--- src/main/java/com/jonas/controller/UserController.java
+++ src/main/java/com/jonas/controller/UserController.java
@@ -2,9 +2,12 @@ package com.jonas.controller;
 
 import com.jonas.api.dto.User;
 import com.jonas.api.service.UserApiService;
+import com.jonas.constant.BizException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,8 +22,8 @@ public class UserController {
     @Autowired
     private UserApiService userApiService;
 
-    @RequestMapping("/getUser")
-    public User getUser(Long uid) {
+    @RequestMapping(value="/getUser")
+    public User getUser(@RequestParam("uid")  Long uid) throws BizException {
         return userApiService.getUser(uid);
     }
 
